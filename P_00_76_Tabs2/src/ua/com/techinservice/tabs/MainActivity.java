c35df6567cfd9930e6d16fc59cee68c8230fd666
KDO <kondrashov.do@gmail.com>
2014-02-25 17:39:20 +0200

    Copy DB, read from DB and write to LOG

--- P_00_76_Tabs2/src/ua/com/techinservice/tabs/MainActivity.java
+++ P_00_76_Tabs2/src/ua/com/techinservice/tabs/MainActivity.java
@@ -1,11 +1,22 @@
 package ua.com.techinservice.tabs;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TabHost;
@@ -13,6 +24,8 @@ import android.widget.TabHost.OnTabChangeListener;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	final String LOG_TAG = "myLogs";
 	
 	//Количество вкладок
 	final int NUMBER_OF_TABS = 5;
@@ -22,6 +35,8 @@ public class MainActivity extends Activity {
 	final String ATTRIBUTE_INPHONE_TEXT = "ATTRIBUTE_INPHONE_TEXT";
 	final String ATTRIBUTE_OUTPHONE_TEXT = "ATTRIBUTE_OUTPHONE_TEXT";
 	
+	DataBaseHelper dbHelper;
+	
 	ListView lvSimple;
 
 	@Override
@@ -29,6 +44,15 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
+		 // создаем объект для создания и управления версиями БД
+	    dbHelper = new DataBaseHelper(this);
+		
+	    try {
+	    	dbHelper.createDataBase();
+	    } catch (IOException ioe) {
+	    	throw new Error("Unable to create database");
+	    }
+	    
 		// массивы данных
 		String[] surname = {"Кабальский", "Ровинский", "Литовченко", "Каравай"};
 		String[] name = {"Геннадий Витальевич", "Артем Демьянович", "Марина Александровна", "Евгения Геннадиевна"};
@@ -68,7 +92,7 @@ public class MainActivity extends Activity {
         
         for (int i = 1; i <= NUMBER_OF_TABS; i++ ) {
 			// создаем вкладку и указываем тег
-			tabSpec = tabHost.newTabSpec("tag"+i);
+			tabSpec = tabHost.newTabSpec("tab"+i);
 			// название вкладки
 			tabSpec.setIndicator("Вкладка "+i);
 			// указываем id компонента из FrameLayout, он и станет содержимым
@@ -78,14 +102,42 @@ public class MainActivity extends Activity {
 		}		
         
         // вторая вкладка будет выбрана по умолчанию
-        tabHost.setCurrentTabByTag("tag2");
+        tabHost.setCurrentTabByTag("tab2");
         
         // обработчик переключения вкладок
         tabHost.setOnTabChangedListener(new OnTabChangeListener() {
-        public void onTabChanged(String tabId) {
-        Toast.makeText(getBaseContext(), "tabId = " + tabId, Toast.LENGTH_SHORT).show();
-      }
-    });
+        	public void onTabChanged(String tabId) {
+        		Toast.makeText(getBaseContext(), "tabId = " + tabId, Toast.LENGTH_SHORT).show();
+        		//подключаемся к БД
+        		SQLiteDatabase db = dbHelper.getWritableDatabase();
+        		Log.d(LOG_TAG, "--- Rows in mytable: ---");
+        	      // делаем запрос всех данных из таблицы mytable, получаем Cursor 
+        	      Cursor c = db.query("phones", null, null, null, null, null, null);
+        	   // ставим позицию курсора на первую строку выборки
+        	      // если в выборке нет строк, вернется false
+        	      if (c.moveToFirst()) {
+
+        	        // определяем номера столбцов по имени в выборке
+        	        int idColIndex = c.getColumnIndex("_id");
+        	        int surnameColIndex = c.getColumnIndex("surname");
+        	        int nameColIndex = c.getColumnIndex("name");
+
+        	        do {
+        	          // получаем значения по номерам столбцов и пишем все в лог
+        	          Log.d(LOG_TAG,
+        	              "ID = " + c.getInt(idColIndex) + 
+        	              ", surname = " + c.getString(surnameColIndex) + 
+        	              ", name = " + c.getString(nameColIndex));
+        	          // переход на следующую строку 
+        	          // а если следующей нет (текущая - последняя), то false - выходим из цикла
+        	        } while (c.moveToNext());
+        	      } else
+        	        Log.d(LOG_TAG, "0 rows");
+        	      c.close();
+        	      // закрываем подключение к БД
+        	      dbHelper.close();
+            }
+        });
         
 	}
 
@@ -95,5 +147,114 @@ public class MainActivity extends Activity {
 //		getMenuInflater().inflate(R.menu.main, menu);
 //		return true;
 //	}
+}
 
+class DataBaseHelper extends SQLiteOpenHelper{
+	// путь к базе данных вашего приложения
+    private static String DB_PATH = "/data/data/ua.com.techinservice.tabs/databases/";
+    private static String DB_NAME = "tech";
+    private SQLiteDatabase myDataBase;
+    private final Context mContext;
+    
+    /**
+     * Конструктор
+     * Принимает и сохраняет ссылку на переданный контекст для доступа к ресурсам приложения
+     * @param context
+     */
+    public DataBaseHelper(Context context) {
+    	super(context, DB_NAME, null, 1);
+        this.mContext = context;
+    }
+    
+    /**
+     * Создает пустую базу данных и перезаписывает ее нашей собственной базой
+     * */
+    public void createDataBase() throws IOException{
+    	boolean dbExist = checkDataBase();
+
+    	if(dbExist){
+    		//ничего не делать - база уже есть
+    	}else{
+    		//вызывая этот метод создаем пустую базу, позже она будет перезаписана
+        	this.getReadableDatabase();
+
+        	try {
+    			copyDataBase();
+    		} catch (IOException e) {
+        		throw new Error("Error copying database");
+        	}
+    	}
+    }
+    
+    /**
+     * Проверяет, существует ли уже эта база, чтобы не копировать каждый раз при запуске приложения
+     * @return true если существует, false если не существует
+     */
+    private boolean checkDataBase(){
+    	SQLiteDatabase checkDB = null;
+
+    	try{
+    		String myPath = DB_PATH + DB_NAME;
+    		checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+    	}catch(SQLiteException e){
+    		//база еще не существует
+    	}
+    	if(checkDB != null){
+    		checkDB.close();
+    	}
+    	return checkDB != null ? true : false;
+    }
+    
+    /**
+     * Копирует базу из папки assets заместо созданной локальной БД
+     * Выполняется путем копирования потока байтов.
+     * */
+    private void copyDataBase() throws IOException{
+    	//Открываем локальную БД как входящий поток
+    	InputStream myInput = mContext.getAssets().open(DB_NAME);
+
+    	//Путь ко вновь созданной БД
+    	String outFileName = DB_PATH + DB_NAME;
+
+    	//Открываем пустую базу данных как исходящий поток
+    	OutputStream myOutput = new FileOutputStream(outFileName);
+
+    	//перемещаем байты из входящего файла в исходящий
+    	byte[] buffer = new byte[1024];
+    	int length;
+    	while ((length = myInput.read(buffer))>0){
+    		myOutput.write(buffer, 0, length);
+    	}
+
+    	//закрываем потоки
+    	myOutput.flush();
+    	myOutput.close();
+    	myInput.close();
+    }
+    
+    public void openDataBase() throws SQLException{
+    	//открываем БД
+        String myPath = DB_PATH + DB_NAME;
+    	myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+    }
+    
+    @Override
+	public synchronized void close() {
+    	    if(myDataBase != null)
+    		    myDataBase.close();
+    	    super.close();
+	}
+    
+    @Override
+	public void onCreate(SQLiteDatabase db) {
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	}
+
+    // Здесь можно добавить вспомогательные методы для доступа и получения данных из БД
+    // вы можете возвращать курсоры через "return myDataBase.query(....)", это облегчит их использование
+    // в создании адаптеров для ваших view
+    
 }
