91a8795c243994813f3e4fb4e0c6a3d8760395d5
KDO <kondrashov.do@gmail.com>
2014-03-04 15:25:35 +0200

    Send outPhone Uri and start new Activity with phone dialer

--- P_00_76_Tabs2/src/ua/com/techinservice/tabs/MainActivity.java
+++ P_00_76_Tabs2/src/ua/com/techinservice/tabs/MainActivity.java
@@ -6,8 +6,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -44,31 +46,36 @@ public class MainActivity extends Activity {
 	
 	DataBaseHelper dbHelper;
 	
-	ListView lvSimple, lvAdministrative;
+	//ListView lvSimple, lvAdministrative;
+	//Массив списков
+	ListView[] lvArray = new ListView[NUMBER_OF_TABS];
 	
+	Intent intent;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
 		TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+		// инициализация вкладок
+        tabHost.setup();
+        TabHost.TabSpec tabSpec;
 		
-		 // создаем объект для создания и управления версиями БД
+		// создаем объект для создания и управления версиями БД
 	    dbHelper = new DataBaseHelper(this);
 		
 	    try {
 	    	dbHelper.createDataBase();
 	    } catch (IOException ioe) {
 	    	throw new Error("Unable to create database");
-	    }
-	    
+	    }	    
 		// массивы данных
 //		String[] surname = {"Кабальский", "Ровинский", "Литовченко", "Каравай"};
 //		String[] name = {"Геннадий Витальевич", "Артем Демьянович", "Марина Александровна", "Евгения Геннадиевна"};
 //		String[] inPhone = {"134", "222", "143", "104"};
 //		String[] outPhone = {"050-351-14-96", "050-312-92-12", "068-701-72-41", "063-145-97-68"};
 
-	    
+	    //Заполняем данные из БД в листы и создаем вкладки
         for (int i = 0; i < NUMBER_OF_TABS; i++) {
 	    	//подключаемся к БД
 			SQLiteDatabase db = dbHelper.getWritableDatabase();
@@ -113,27 +120,10 @@ public class MainActivity extends Activity {
 		    // создаем адаптер
 	        SimpleAdapter sAdapter = new SimpleAdapter(this, data, R.layout.my_list_item, from, to);
 	        // определяем список и присваиваем ему адаптер
-	        lvSimple = (ListView) findViewById(0x7f070000 + i);
-	        lvSimple.setAdapter(sAdapter);
-	    }
-        // onClick administrative List (Tab1)
-        lvAdministrative = (ListView) findViewById(R.id.lvTab1);
-        lvAdministrative.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                int position, long id) {
-            	String outPhone = ((TextView) view.findViewById(R.id.tvOutPhone)).getText().toString();
-            	
-            	//parent.getAdapter().getItem(position).toString()
-              Log.d(LOG_TAG, "itemClick: position = " + position + ", id = "
-                  + id + ", outPhone = " + outPhone);
-            }
-          });
-        
-        // инициализация вкладок
-        tabHost.setup();
-        TabHost.TabSpec tabSpec;
-        
-        for (int i = 0; i < NUMBER_OF_TABS; i++ ) {
+	        lvArray[i] = (ListView) findViewById(0x7f070000 + i);
+	        lvArray[i].setAdapter(sAdapter);
+	        
+	        // СОЗДАНИЕ ВКЛАДОК!!!
 			// создаем вкладку и указываем тег
 			tabSpec = tabHost.newTabSpec("tab" + i);
 			// название вкладки
@@ -142,8 +132,8 @@ public class MainActivity extends Activity {
 			tabSpec.setContent(0x7f070000 + i);
 			// добавляем в корневой элемент
 			tabHost.addTab(tabSpec);
-		}		
-        
+	    }
+  
         // вторая вкладка будет выбрана по умолчанию
         tabHost.setCurrentTabByTag("tab2");
 
@@ -152,9 +142,6 @@ public class MainActivity extends Activity {
         	public void onTabChanged(String tabId) {
         		
         		Toast.makeText(getBaseContext(), "tabId = " + tabId, Toast.LENGTH_SHORT).show();
-        		
-//        		//подключаемся к БД
-//        		SQLiteDatabase db = dbHelper.getWritableDatabase();
         		
         		if (tabId.equals("tab0")) {
         			
@@ -165,43 +152,34 @@ public class MainActivity extends Activity {
         		}
         		
         		if (tabId.equals("tab2")) {
-        			
+        		
         		}
         		
         		if (tabId.equals("tab3")) {
-        			
+        		
         		}
         		
         		if (tabId.equals("tab4")) {
-//        			Log.d(LOG_TAG, "--- Rows in mytable: ---");
-//          	      // делаем запрос всех данных из таблицы mytable, получаем Cursor 
-//          	      Cursor c = db.query("phones", null, null, null, null, null, null);
-//          	      // ставим позицию курсора на первую строку выборки
-//          	      // если в выборке нет строк, вернется false
-//          	      if (c.moveToFirst()) {
-//
-//          	        // определяем номера столбцов по имени в выборке
-//          	        int idColIndex = c.getColumnIndex("_id");
-//          	        int surnameColIndex = c.getColumnIndex("surname");
-//          	        int nameColIndex = c.getColumnIndex("name");
-//
-//          	        do {
-//          	          // получаем значения по номерам столбцов и пишем все в лог
-//          	          Log.d(LOG_TAG,
-//          	              "ID = " + c.getInt(idColIndex) + 
-//          	              ", surname = " + c.getString(surnameColIndex) + 
-//          	              ", name = " + c.getString(nameColIndex));
-//          	          // переход на следующую строку 
-//          	          // а если следующей нет (текущая - последняя), то false - выходим из цикла
-//          	        } while (c.moveToNext());
-//          	      } else
-//          	        Log.d(LOG_TAG, "0 rows");
-//          	      c.close();
+        		
         		}
-//        		// закрываем подключение к БД
-//        		dbHelper.close();
             }
         });
+        
+     // обработчик нажатия на елемент в списке
+        lvArray[1].setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                int position, long id) {
+            	String outPhone = ((TextView) view.findViewById(R.id.tvOutPhone)).getText().toString();
+            	
+            	//parent.getAdapter().getItem(position).toString()
+              Log.d(LOG_TAG, "itemClick: position = " + position + ", id = "
+                  + id + ", outPhone = " + outPhone);
+              
+              intent = new Intent(Intent.ACTION_DIAL);
+              intent.setData(Uri.parse("tel:" + outPhone));
+              startActivity(intent);
+            }
+          });
         
 	}
 
